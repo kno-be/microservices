6b98e0e867d8e9a89f8fa3dab5737e4917f2001c
Bernardo Knoblauch <beknoblauch18@gmail.com>
2022-01-26 17:29:26 -0300

    upd: tailwind implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,11 @@
-import Reac
+import React from "react";
+import './index.css'
+import PostCreate from './PostCreate'
+
+const App = () => {
+    return <div>
+        <PostCreate/>
+    </div>
+}
+
+export default App;
---
+++ comments/index.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+const app = express();
+
+app.get('/comments', (req, res) => {
+
+})
+
+app.post('/comments',(req,res) => {
+
+})
+
+app.listen(4001, () => {
+    console.log('Listening on 4001...')
+})
